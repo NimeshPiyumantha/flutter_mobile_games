4b7404c0d1ee85ff3c4c5e7ce6cff1c8c5073462
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2025-05-22 18:47:56 +0530

    update code

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -3,5 +3,5 @@ class Assets {
 
   static const String scratchPattern = 'assets/images/scratch_pattern.png';
   static const String winLog = 'assets/logo/winlads_splash_logo.png';
-  static const String scratchModel='assets/images/scratch.png';
+  static const String scratchModel = 'assets/images/scratch.png';
 }
